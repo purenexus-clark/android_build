46d1471fcbaa759a7f05ae71525ab87cf7357c1a
HashBang <hashbang173@gmail.com>
2016-03-04 17:14:15 -0500

    add back stuff in qcom_target.mk

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -1,5 +1,20 @@
 # Target-specific configuration
 
+define bt-vendor-set-path-variant
+$(call project-set-path-variant,bt-vendor,TARGET_BT_VENDOR_VARIANT,hardware/qcom/$(1))
+endef
+
+# Set device-specific HALs into project pathmap
+define set-device-specific-path
+$(if $(USE_DEVICE_SPECIFIC_$(1)), \
+    $(if $(DEVICE_SPECIFIC_$(1)_PATH), \
+        $(eval path := $(DEVICE_SPECIFIC_$(1)_PATH)), \
+        $(eval path := $(TARGET_DEVICE_DIR)/$(2))), \
+    $(eval path := $(3))) \
+$(call project-set-path,qcom-$(2),$(strip $(path)))
+endef
+
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
     COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
@@ -9,4 +24,8 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
         COMMON_GLOBAL_CFLAGS += -DQCOM_BSP -DQTI_BSP
         COMMON_GLOBAL_CPPFLAGS += -DQCOM_BSP -DQTI_BSP
     endif
+$(call bt-vendor-set-path-variant,bt-caf)
+else
+$(call bt-vendor-set-path-variant,bt)
 endif
+
